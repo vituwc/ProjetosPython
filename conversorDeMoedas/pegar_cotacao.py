0783278e52afe7eb96116e2fe00e48512caab3fa
victor <147680227+vituwc@users.noreply.github.com>
2024-11-11 14:21:31 -0300

    Apenas realocando os projetos

---
+++ conversorDeMoedas/pegar_cotacao.py
@@ -0,0 +1,11 @@
+import requests
+
+def pegar_cotacao_moeda(moeda_origem, moeda_destino):
+    link = f"https://economia.awesomeapi.com.br/last/{moeda_origem}-{moeda_destino}"
+    requisicao = requests.get(link)
+
+    cotacao = requisicao.json()[f"{moeda_origem}{moeda_destino}"]["bid"]
+    return cotacao
+
+
+# 200 - requisição funcionou
